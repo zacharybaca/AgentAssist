5476f1b8c0dc9bbe017e1db572ffbe3b6d8580d4
Zachary Baca <zachary.baca@msn.com>
2025-04-16 07:24:39 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ArticlesProvider } from './context/ArticlesContext.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <Router>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <ArticlesProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </ArticlesProvider>
   </Router>
   ,
 )
