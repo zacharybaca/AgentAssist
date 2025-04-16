ee709a9b32f4a17d0802be8467c397cbf6b627a2
Zachary Baca <zachary.baca@msn.com>
2025-04-16 10:03:31 -0500

    Update ArticleList.jsx

--- client/src/components/ArticleList/ArticleList.jsx
+++ client/src/components/ArticleList/ArticleList.jsx
@@ -1,9 +1,22 @@
-import './article-list.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { useArticles } from '../context/ArticlesContext';
+import ConfirmModal from './ConfirmModal';
 
 const ArticleList = ({ onEdit }) => {
   const { articles, deleteArticle } = useArticles();
+  const [selectedId, setSelectedId] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleDeleteClick = (id) => {
+    setSelectedId(id);
+    setIsModalOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    await deleteArticle(selectedId);
+    setIsModalOpen(false);
+    setSelectedId(null);
+  };
 
   return (
     <div className="max-w-3xl mx-auto p-4">
@@ -20,7 +33,7 @@ const ArticleList = ({ onEdit }) => {
                 Edit
               </button>
               <button
-                onClick={() => deleteArticle(article._id)}
+                onClick={() => handleDeleteClick(article._id)}
                 className="text-sm text-red-600 hover:underline"
               >
                 Delete
@@ -29,6 +42,13 @@ const ArticleList = ({ onEdit }) => {
           </li>
         ))}
       </ul>
+
+      <ConfirmModal
+        isOpen={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        onConfirm={confirmDelete}
+        message="Are you sure you want to delete this article?"
+      />
     </div>
   );
 };
