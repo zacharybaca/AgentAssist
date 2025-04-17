762ad77650c29ba8246ae08bde53f756c271ef26
Zachary Baca <zachary.baca@msn.com>
2025-04-17 12:30:36 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { AppProviders } from "./context/AppProvider.jsx";
 import App from './App.jsx';
 import './index.css';
 
-import { AuthProvider } from './context/AuthProvider.jsx';
-import { ArticlesProvider } from './context/ArticlesProvider.jsx';
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <AuthProvider>
-        <ArticlesProvider>
-          <App />
-        </ArticlesProvider>
-      </AuthProvider>
-    </Router>
+    <AppProviders>
+      <App />
+    </AppProviders>
   </StrictMode>
 );
