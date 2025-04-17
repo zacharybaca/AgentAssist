dba1aace2425fef83444db4cddd2af454ba45d58
zacharybaca <zachary.baca@msn.com>
2025-04-17 05:54:28 -0500

    configured some files and added loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,26 +4,29 @@ import { useNavigate, Routes, Route } from 'react-router-dom';
 import ArticleForm from "./components/AdminPanel/ArticleForm/ArticleForm";
 import AdminArticlesView from "./components/AdminPanel/AdminArticlesView/AdminArticlesView.jsx";
 import { Toaster } from 'react-hot-toast';
+import Logo from './assets/agent-assist-icon-no-background.png';
 
 function App() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const loader = document.getElementById("loader");
+  const loaderContainer = document.getElementById("loader-container");
 
   useEffect(() => {
-    if (loader) {
+    if (loader && loaderContainer) {
       setTimeout(() => {
         loader.style.display = 'none';
+        loaderContainer.style.display = 'none';
         setLoading(false);
       }, 6000);
     }
-  }, [loader]);
+  }, [loader, loaderContainer]);
 
   return (
     !loading && (
       <div id="main-app-container">
         <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
-        <h1>Agent Assist</h1>
+        <img src={Logo} alt="logo" />
         <Routes>
           <Route path="/admin/article-form" element={<ArticleForm onSuccess={() => {
             navigate('/articles');
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5173",
+        target: "http://localhost:9000",
         changeOrigin: true,
         secure: false,
       },
