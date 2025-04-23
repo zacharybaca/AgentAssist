c5c9314020b1ace6b66d28d41520cc3513f7e0fb
Zachary Baca <zachary.baca@msn.com>
2025-04-23 12:56:36 -0500

    updated article rating component

--- client/src/components/ArticleRating/ArticleRating.jsx
+++ client/src/components/ArticleRating/ArticleRating.jsx
@@ -1,10 +1,20 @@
 import './article-rating.css';
+import { Star, StarHalf } from "lucide-react";
 
 const ArticleRating = () => {
 
     return (
-        <div id="rating-container">
-            Rating
+        <div className="rating-container">
+            <div className="stars-container">
+                { Array.from({ length: 5 }, () => (
+                <Star fill="#111" strokeWidth={0} />
+                ))}
+            </div>
+            <div className="stars rating">
+                <Star fill="yellow" strokeWidth={0} />
+                <Star fill="yellow" strokeWidth={0} />
+                <StarHalf fill="yellow" strokeWidth={0} />
+            </div>
         </div>
     )
 }
