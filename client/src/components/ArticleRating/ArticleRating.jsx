0cf4dcf4d9d560736c8c8c391d63eac0686ccedb
Zachary Baca <zachary.baca@msn.com>
2025-04-25 10:23:45 -0500

    Update ArticleRating.jsx

--- client/src/components/ArticleRating/ArticleRating.jsx
+++ client/src/components/ArticleRating/ArticleRating.jsx
@@ -3,12 +3,30 @@ import { useState } from 'react';
 import { Star, StarHalf } from "lucide-react";
 
 const ArticleRating = () => {
-    
+    const [hasVoted, setHasVoted] = useState(false);
+    const [totalRatings, setTotalRatings] = useState(0);
+    const [userStarRatingArray, setUserStarRatingArray] = useState([]);
+    const [averageStarRatingArray, setAverageStarRatingArray] = useState([]);
+    const [numberOfRatings, setNumberOfRatings] = useState(0);
     const [rating, setRating] = useState(0);
 
     const handleStarClick = (selectedRating) => {
+        if (hasVoted) return; 
+      
         setRating(selectedRating);
-      };
+        setUserStarRatingArray(createRatingArray(selectedRating));
+        setTotalRatings(prevTotal => {
+          const newTotal = prevTotal + selectedRating;
+          const newCount = numberOfRatings + 1;
+          const avg = Math.round(newTotal / newCount);
+          setAverageStarRatingArray(createRatingArray(avg));
+          return newTotal;
+        });
+        setNumberOfRatings(prev => prev + 1);
+        setHasVoted(true); 
+      };       
+      
+    const createRatingArray = (userRating) => Array.from({ length: userRating }, (_, i) => i + 1);
 
       return (
         <div className="rating-container">
@@ -25,7 +43,21 @@ const ArticleRating = () => {
               )}
             </span>
           ))}
-          <p>Rating: {rating}</p>
+          <p>Rating: {userStarRatingArray.map((index) => (
+            <span
+                key={index}
+            >
+                <Star fill="gold" />
+            </span>
+          ))}</p>
+          <p>Average Rating: {averageStarRatingArray.map((index) => (
+            <span
+                key={index}
+            >
+                <Star fill="gold" />
+            </span>
+          ))}</p>
+          <p>Total Ratings: {totalRatings}</p>
         </div>
       );
 }
