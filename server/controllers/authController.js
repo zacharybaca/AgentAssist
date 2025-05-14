31bda59a1e40ff917d1333e8165c651f375d71b0
Zachary Baca <zachary.baca@msn.com>
2025-05-14 08:49:03 -0500

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,23 +1,28 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
-export const registerAdmin = async (req, res) => {
+export const register = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { name, username, email, password, role, favoriteArticles, avatar } = req.body;
 
-    // Check if admin already exists (optional safeguard)
-    const existingAdmin = await User.findOne({ role: "admin" });
-    if (existingAdmin) {
-      return res.status(403).json({ message: "Admin account already exists." });
+    // Check if user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(403).json({ message: "Account already exists." });
     }
 
-    const newUser = new User({ username, password, role: "admin" });
+    const newUser = new User({ name, username, email, password, role, favoriteArticles, avatar });
+
+    if (!name || !username || !email || !password || !role) {
+      return res.status(403).json({ message: "Please provide required data for all required fields"});
+    }
+    
     await newUser.save();
 
-    res.status(201).json({ message: "Admin account created successfully." });
+    res.status(201).json({ message: "Account created successfully." });
   } catch (err) {
-    console.error("Error creating admin:", err);
-    res.status(500).json({ message: "Server error while registering admin." });
+    console.error("Error creating account:", err);
+    res.status(500).json({ message: "Server error while registering account." });
   }
 };
 
