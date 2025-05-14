0a9b4c2dc948d8004cec38bc34ee9db74f4be57f
Zachary Baca <zachary.baca@msn.com>
2025-05-14 09:02:56 -0500

    Update Agent.js

--- server/models/Agent.js
+++ server/models/Agent.js
@@ -1,11 +1,15 @@
 // models/User.js
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import validator from "validator";
 
 const agentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   username: { type: String, required: true, unique: true },
-  email: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true, validate: {
+      validator: validator.isEmail,
+      message: "Please enter a valid email address",
+    }, },
   password: { type: String, required: true },
   role: {
     type: String,
