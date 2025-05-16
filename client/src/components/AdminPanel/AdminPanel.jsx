a6f491becad0fd90412acc20341e9bda2c5f4214
Zachary Baca <zachary.baca@msn.com>
2025-05-16 13:35:43 -0500

    Update AdminPanel.jsx

--- client/src/components/AdminPanel/AdminPanel.jsx
+++ client/src/components/AdminPanel/AdminPanel.jsx
@@ -5,8 +5,10 @@ const AdminPanel = () => {
 
     return (
         <div id="admin-panel-container">
-            <Link to ='/admin/articles'>Admin Articles View</Link>
+            <Link to ='/admin/articles'>Manage Articles</Link>
             <Link to='/admin/articles/create'>Create An Article</Link>
+            <Link to='/admin/categories'>Manage Categories</Link>
+            <Link to='/admin/categories/create'>Create A Category</Link>
         </div>
     )
 }
