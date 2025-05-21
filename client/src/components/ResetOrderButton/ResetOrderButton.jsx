34af4f6840d21b4d12b45103a8c625ace75215ba
Zachary Baca <zachary.baca@msn.com>
2025-05-21 08:43:45 -0500

    Create ResetOrderButton.jsx

---
+++ client/src/components/ResetOrderButton/ResetOrderButton.jsx
@@ -0,0 +1,12 @@
+import './reset-order-button.css';
+
+const ResetOrderButton = () => {
+
+    return (
+        <div>
+            <button onClick={resetOrder}>Reset Order</button>
+        </div>
+    )
+}
+
+export default ResetOrderButton;
