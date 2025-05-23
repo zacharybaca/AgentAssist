8fce7a13b2bad2962f33af6c17d9316a38a42e10
Zachary Baca <zachary.baca@msn.com>
2025-05-23 13:07:22 -0500

    Update Article.js

--- server/models/Article.js
+++ server/models/Article.js
@@ -31,6 +31,10 @@ const articleSchema = new mongoose.Schema({
     enum: ["draft", "published"],
     default: "draft",
   },
+  favoritedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Agent",
+  },
 });
 
 const Article = mongoose.model("Article", articleSchema);
