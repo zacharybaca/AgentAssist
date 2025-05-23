de09566393767eae6640a2bec920e2cfa5f9e5d0
Zachary Baca <zachary.baca@msn.com>
2025-05-23 06:57:09 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import "react-quill/dist/quill.snow.css";
+import 'quill/dist/quill.snow.css';
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
