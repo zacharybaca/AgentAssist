fe75bdc511e547287e59f1ec6a45a8671f2f3cd4
Zachary Baca <zachary.baca@msn.com>
2025-06-04 08:14:34 -0500

    Create AvatarSelector.jsx

---
+++ client/src/components/AvatarSelector/AvatarSelector.jsx
@@ -0,0 +1,18 @@
+import './avatar-selector.css';
+import React from 'react';
+
+export default function AvatarSelector({ avatars = [], selectedAvatar, onSelect }) {
+    return (
+        <div className="avatar-grid">
+            {avatars.map((url) => (
+                <img
+                    key={url}
+                    src={url}
+                    alt="Predefined avatar"
+                    className={`avatar-option ${selectedAvatar === url ? 'selected' : ''}`}
+                    onClick={() => onSelect(url)}
+                />
+            ))}
+        </div>
+    );
+}
