21b8a65427fe3c5fd130ae8bca44219202fa86a9
Zachary Baca <zachary.baca@msn.com>
2025-06-04 09:56:35 -0500

    Update AvatarUpload.jsx

--- client/src/components/AvatarUpload/AvatarUpload.jsx
+++ client/src/components/AvatarUpload/AvatarUpload.jsx
@@ -67,7 +67,6 @@ export default function AvatarUpload({ onUploadComplete }) {
     };
 
     const predefinedAvatars = [
-        '',
         HeadsetAvatar,
         FemaleOperatorTwoAvatar,
         FemaleOperatorAvatar,
